9d7e659d0b48d766f0e4c92ba5816cdb5bb8ca4b
zhengyan <haha110099@gmail.com>
2021-04-14 15:37:10 +0800

    指定允許接收受的型態

--- Flask/main.py
+++ Flask/main.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
 
 # @app.route('/hello')
 # def hello():
@@ -21,3 +21,10 @@ def helloname1(name):
 @app.route('/', methods=['POST', 'GET'])
 def hello_world1():      #宣告名hello_world不能重複
     return 'Hello, World!'
+
+@app.route('/<int:num>')
+def get_integer(num):
+    return 'Integer: {}'.format(num)
+@app.route('/<float:num>')
+def get_float(num):
+    return 'Float: {}'.format(num)
