391e8c13da888b3f9bf847f0450358824d35d0e9
zhengyan <haha110099@gmail.com>
2021-04-14 16:04:08 +0800

    Flask 使用 Jinja2 模板引擎 建template資料夾 加html

--- Flask/main.py
+++ Flask/main.py
@@ -38,8 +38,10 @@
 #     return 'Hello, {}!'.format(name)
 
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Hello, world!</h1>'
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('index.html', name=name)
