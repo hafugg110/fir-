f93450f80b6751a3b52d793733737b3cb36789a9
zhengyan <haha110099@gmail.com>
2021-04-14 15:38:25 +0800

    利用網址傳遞參數

--- Flask/main.py
+++ Flask/main.py
@@ -1,30 +1,37 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
+# # @app.route('/')
+# # def hello_world():
+# #     return 'Hello, World!'
 
-# @app.route('/hello')
-# def hello():
-#     return 'Hello!'
+# # @app.route('/hello')
+# # def hello():
+# #     return 'Hello!'
 
+# # @app.route('/hello/<name>')
+# # def helloname(name):   #宣告名help不能重複
+# #     return 'Hello, {}!'.format(name)
+
+# @app.route('/hello', defaults={'name': 'Someone'})
 # @app.route('/hello/<name>')
-# def helloname(name):   #宣告名help不能重複
+# def helloname1(name):
 #     return 'Hello, {}!'.format(name)
 
-@app.route('/hello', defaults={'name': 'Someone'})
-@app.route('/hello/<name>')
-def helloname1(name):
+# @app.route('/', methods=['POST', 'GET'])
+# def hello_world1():      #宣告名hello_world不能重複
+#     return 'Hello, World!'
+
+# @app.route('/<int:num>')
+# def get_integer(num):
+#     return 'Integer: {}'.format(num)
+# @app.route('/<float:num>')
+# def get_float(num):
+#     return 'Float: {}'.format(num)
+
+from flask import Flask, request
+
+@app.route('/query')
+def query():
+name = request.args.get('name')
     return 'Hello, {}!'.format(name)
-
-@app.route('/', methods=['POST', 'GET'])
-def hello_world1():      #宣告名hello_world不能重複
-    return 'Hello, World!'
-
-@app.route('/<int:num>')
-def get_integer(num):
-    return 'Integer: {}'.format(num)
-@app.route('/<float:num>')
-def get_float(num):
-    return 'Float: {}'.format(num)
