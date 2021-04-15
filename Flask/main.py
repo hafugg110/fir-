aa8ea98e706e4b2f187a785b825b602fe0da88bb
zhengyan <haha110099@gmail.com>
2021-04-15 09:57:58 +0800

    if else {{ }} 引用變數或表示式

--- Flask/main.py
+++ Flask/main.py
@@ -41,6 +41,9 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+@app.route('/', methods=['POST', 'GET'])
+def hello_world():
+    return 'Hello, World!'
 @app.route('/hello')
 @app.route('/hello/<name>')
 def hello(name=None):
