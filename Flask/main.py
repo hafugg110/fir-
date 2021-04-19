5168c84954ee2f9b93cbd1da5834de5c7bb87089
zhengyan <haha110099@gmail.com>
2021-04-19 11:24:33 +0800

    實作一個 ToDo List 定義現有清單 新增一個 route 增加 toDo 項目

--- Flask/main.py
+++ Flask/main.py
@@ -64,24 +64,24 @@
 #     return render_template('img.html')
 
 
-from flask import Flask, render_template
-app = Flask(__name__)
+# from flask import Flask, render_template
+# app = Flask(__name__)
 
-@app.route('/')
-def show_table():
-    basic_info = [
-        {
-            'name': 'Amber',
-            'phone': 'Amber phone',
-            'address': 'Amber address'
-        },
-        {
-            'name': 'Peter',
-            'phone': 'Peter phone',
-            'address': 'Peter address'
-        }
-    ]
-    return render_template("index.html", data=basic_info)
+# @app.route('/')
+# def show_table():
+#     basic_info = [
+#         {
+#             'name': 'Amber',
+#             'phone': 'Amber phone',
+#             'address': 'Amber address'
+#         },
+#         {
+#             'name': 'Peter',
+#             'phone': 'Peter phone',
+#             'address': 'Peter address'
+#         }
+#     ]
+#     return render_template("index.html", data=basic_info)
 
 # from flask import Flask, render_template, request
 # app = Flask(__name__)
@@ -105,3 +105,52 @@ def show_table():
 #     firstname = request.values['firstname']
 #     lastname = request.values['lastname']
 #     return render_template('submit.html',**locals())
+
+import json
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+tasks = {
+    0:{
+        'id': 0,
+        'description': 'todo-1',
+        'done': False
+    },
+    1:{
+        'id': 1,
+        'description': 'todo-2',
+        'done': False
+    }
+}
+#定義清單計數器
+take_id_coounter = 2
+
+@app.route('/')
+@app.route('/tasks/')
+def get_tasks():
+    res = {
+        'success':True,
+        'data':list(tasks.values())
+    }
+    return jsonify(res)
+
+#新增todo項目
+@app.route('/tasks/', methods=['POST'])
+def create_task():
+    #讓計數器可以被全域讀取
+    global task_id_counter
+    #將取得的資料以 json 載入
+    body = json.loads(request.data)
+    #指定拿取 description , 若無則顯示 no description
+    description = body.get("description", "no description")
+    #開始建立新的清單內容
+    task = {"id": take_id_coounter,
+            "description": description,    
+            "done": False}
+    #將清單內容增加回 task 清單中
+    tasks[take_id_coounter] = task
+    take_id_coounter += 1
+    return json.dumps({
+        "success": True,
+        "data": task
+    }), 201
