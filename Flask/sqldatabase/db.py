fda49db2991a3c2cdb6b298576905a0149ff2923
zhengyan <haha110099@gmail.com>
2021-04-27 13:38:27 +0800

    回暫存2

--- Flask/sqldatabase/db.py
+++ Flask/sqldatabase/db.py
@@ -2,7 +2,7 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print("Opened database successfully")
 
-conn.ececute(
+conn.execute(
     """
     CREATE TABLE students (
         name TEXT,
@@ -14,24 +14,3 @@ conn.ececute(
 print("Table created successfully")
 conn.colne()
 
-from flask import Flask, render_template, url_for, request
-import sqlite3 as sql
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/enternew')
-def new_student():
-    return render_template(('student.html'))
-
-@app.route('/addrec', methods=['POST','GET'])
-def addrec():
-    if request.method == 'POST':
-        try:
-            nm = request.form['nm']
-            addr = request.form['add']
-            city = request.form['city']
-            pin
