79df986a2415b62743edbd2a249c9edb7ad20e50
zhengyan <haha110099@gmail.com>
2021-05-05 11:24:13 +0800

    orm暫存

--- webapplication/sql/sql_orm_tempaltes/app.py
+++ webapplication/sql/sql_orm_tempaltes/app.py
@@ -1,4 +1,3 @@
-# 定義初始參數
 import json
 from flask import Flask, request
 from db import db
@@ -19,17 +18,10 @@ with app.app_context():
     db.create_all()
 
 # reponse format
-
-
 def success_response(data, code=200):
     return json.dumps({"success": True, "data": data}), code
-
-
 def failure_response(message, code=404):
     return json.dumps({"success": False, "error": message}), code
-
-# 定義查詢路由
-
 
 @app.route("/")
 @app.route("/tasks/")
@@ -37,20 +29,15 @@ def get_tasks():
     tasks = [t.serialize() for t in Task.query.all()]
     return success_response(tasks)
 
-
 @app.route("/subtasks/")
 def get_subtasks():
     subtask = [s.serialize() for s in Subtask.query.all()]
     return success_response(subtask)
 
-
 @app.route("/categories/")
 def categories():
     category = [c.serialize() for c in Category.query.all()]
     return success_response(category)
-
-# Create a task
-
 
 @app.route("/tasks/", methods=["POST"])
 def create_task():
@@ -58,37 +45,31 @@ def create_task():
 
     new_task = Task(
         description=body.get('description', ''),
-        done=body.get('done', False)
-        # 無法取得數值時，給予初始值避免出錯
-    )
+done=body.get('done', False)
+# 無法取得數值時，給予初始值避免出錯
+        )
 
     db.session.add(new_task)
     db.session.commit()
     return success_response(new_task.serialize(), 201)
 
-# 查詢特定 id 之 task
-
-
 @app.route("/tasks/<int:task_id>/")
 def get_task(task_id):
     task = Task.query.filter_by(id=task_id).first()
-    # first() 取出查詢到的第一行資料
+	# first() 取出查詢到的第一行資料
 
-    # 下面檢查是否沒有取得任何內容
+	# 下面檢查是否沒有取得任何內容
     if task is None:
         return failure_response("Task not found!")
     return success_response(task.serialize())
-
-# 更改特定 id 之 task
-
 
 @app.route("/tasks/<int:task_id>/", methods=["POST"])
 def update_task(task_id):
     task = Task.query.filter_by(id=task_id).first()
     if task is None:
         return failure_response("Task not found!")
-
-    # 取得新輸入的值
+  
+   # 取得新輸入的值
     body = json.loads(request.data)
     task.description = body.get('description', task.description)
     task.done = body.get('done', task.done)
@@ -96,60 +77,15 @@ def update_task(task_id):
     db.session.commit()
     return success_response(task.serialize())
 
-# 刪除特定 id 之 task
-
-
 @app.route("/tasks/<int:task_id>/", methods=["DELETE"])
 def delete_task(task_id):
     task = Task.query.filter_by(id=task_id).first()
     if task is None:
         return failure_response("Task not found!")
-
-    db.session.delete(task)
+  
+    db.session.delete(task)  
     db.session.commit()
     return success_response(task.serialize())
 
 
-# @app.route("/tasks/")
-# def get_tasks():
-#     return success_response(DB.get_all_tasks())
 
-
-# @app.route("/tasks/", methods=["POST"])
-# def create_task():
-#     body = json.loads(request.data)
-#     description = body["description"]
-#     task_id = DB.insert_task_table(description, False)
-#     print(task_id)
-#     task = DB.get_task_by_id(task_id)
-#     print(task)
-#     if task is None:
-#         return failure_response("Something went wrong while creating task!")
-#     return success_response(task, 201)
-
-# @app.route("/tasks/<int:task_id>/")
-# def get_task(task_id):
-#     task = DB.get_task_by_id(task_id)
-#     if task is None:
-#         return failure_response("Task not found!")
-#     return success_response(task)
-
-# @app.route("/tasks/<int:task_id>/", methods=["POST"])
-# def update_task(task_id):
-#     body = json.loads(request.data)
-#     description = body["description"]
-#     done = bool(body["done"])
-#     DB.update_task_by_id(task_id, description, done)
-
-#     task = DB.get_task_by_id(task_id)
-#     if task is None:
-#         return failure_response("Task not found!")
-#     return success_response(task)
-
-# @app.route("/mem_manage/", methods=["DELETE"])
-# def delete_task():
-#     task = DB.get_task_by_id(task_id)
-#     if task is None:
-#         return failure_response("Task not found!")
-#     DB.delete_task_by_id(task_id)
-#     return success_response(task)
